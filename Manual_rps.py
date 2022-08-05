4a1a39e6ccde162075a08a2a842367109a2a5083
David Awoyelu <awoyelu123@gmail.com>
2022-08-05 09:38:35 +0100

    new keras file

--- Manual_rps.py
+++ Manual_rps.py
@@ -1,4 +1,5 @@
 import random
+from requests import options
 
 class RockPaperScissors():
     def __init__(self,computer_choice,user_choice, get_winner):
@@ -6,14 +7,15 @@ class RockPaperScissors():
         self.user_choice = user_choice
         self.get_winner = get_winner
    
-def get_computer_choice(self):
-    options = ["rock","paper","scissors"]
-    self.computer_choice = random.choice(options)
+def get_computer_choice(self,rps_choice):
+    self.rps_choice = ["rock","paper","scissors"]
+    self.computer_choice = random.choice(rps_choice)
 
 def get_user_choice(self):
     self.user_choice = input("Choose rock, paper or scissors. ").lower()
+    self.user_choice not in self.rps_choice
    
-def get_winner(self):
+def get_winner(self,computer_choice, user_choice):
     
     if self.computer_choice == "rock":
         if self.user_choice == "paper":
@@ -39,6 +41,7 @@ def get_winner(self):
             print("It is a draw!")
         else:
             print("You lost!")
+
 
 
         
