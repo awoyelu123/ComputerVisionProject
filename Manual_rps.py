307ee245581f16e01635f14b620412f55bcf9952
David Awoyelu <awoyelu123@gmail.com>
2022-08-05 10:12:08 +0100

    added play game function

--- Manual_rps.py
+++ Manual_rps.py
@@ -1,47 +1,58 @@
 import random
-from requests import options
-
-class RockPaperScissors():
-    def __init__(self,computer_choice,user_choice, get_winner):
-        self.computer_choice = computer_choice
-        self.user_choice = user_choice
-        self.get_winner = get_winner
-   
-def get_computer_choice(self,rps_choice):
-    self.rps_choice = ["rock","paper","scissors"]
-    self.computer_choice = random.choice(rps_choice)
-
-def get_user_choice(self):
-    self.user_choice = input("Choose rock, paper or scissors. ").lower()
-    self.user_choice not in self.rps_choice
-   
-def get_winner(self,computer_choice, user_choice):
-    
-    if self.computer_choice == "rock":
-        if self.user_choice == "paper":
-            print("You won!")
-        elif self.user_choice == "rock":
-            print("It is a draw!")
-        else:
-            print("You lost!")
-
-    if self.computer_choice == "paper":
-        if self.user_choice == "scissors":
-            print("You won!")
-        elif self.user_choice == "paper":
-            print("It is a draw!")
-        else:
-            print("You lost!")
-
-
-    if self.computer_choice == "scissors":
-        if self.user_choice == "rock":
-            print("You won!")
-        elif self.user_choice == "scissors":
-            print("It is a draw!")
-        else:
-            print("You lost!")
+class RockPaperScissors:
 
 
 
+    def __init__(self,rps):
+        self.rps = rps
         
+   
+    def get_computer_choice(self):
+        self.computer_choice = random.choice(rps)
+        self.get_user_choice()
+
+    def get_user_choice(self):
+        self.user_choice = input("Choose rock, paper or scissors. ").lower()
+        if self.user_choice not in self.rps:
+            print("Please choose a valid option.")
+        else:
+            self.get_winner()
+   
+    def get_winner(self):
+        print(f"You chose {self.user_choice}. The computer chose {self.computer_choice}.")
+        
+        if self.computer_choice == "rock":
+            if self.user_choice == "paper":
+                print("You won!")
+            elif self.user_choice == "rock":
+                print("It is a draw!")
+            else:
+                print("You lost!")
+
+        if self.computer_choice == "paper":
+            if self.user_choice == "scissors":
+                print("You won!")
+            elif self.user_choice == "paper":
+                print("It is a draw!")
+            else:
+                print("You lost!")
+
+
+        if self.computer_choice == "scissors":
+            if self.user_choice == "rock":
+                print("You won!")
+            elif self.user_choice == "scissors":
+                print("It is a draw!")
+            else:
+                print("You lost!")
+
+
+
+def play_game(rps):
+
+    game = RockPaperScissors(rps)
+    game.get_computer_choice()
+    
+if __name__ == '__main__':
+    rps = ["rock","paper","scissors"]
+    play_game(rps)
